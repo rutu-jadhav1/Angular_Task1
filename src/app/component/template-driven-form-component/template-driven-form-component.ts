64365a86e20d62f91c686b51f01e3eb41a597b6b
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2025-07-28 02:34:32 +0530

    template driven form

--- src/app/component/template-driven-form-component/template-driven-form-component.ts
+++ src/app/component/template-driven-form-component/template-driven-form-component.ts
@@ -1,11 +1,19 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
+  standalone: true,
   selector: 'app-template-driven-form-component',
-  imports: [],
+  imports: [FormsModule,NgIf],
   templateUrl: './template-driven-form-component.html',
   styleUrl: './template-driven-form-component.css'
 })
 export class TemplateDrivenFormComponent {
+ userDetails: any;
 
+  addDetails(formValue: any) {
+    this.userDetails = formValue;
+    console.log(this.userDetails);
+  }
 }
